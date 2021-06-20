42309707719057c08470f0dd8cc2e8611b7cc9fe
sharon002 <sharonjep2016@gmail.com>
2021-06-20 23:53:33 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Pitch, Comment,UpVote,DownVote
 
 
 # instances for creating the app
-app = create_app('development')
+app = create_app('production')
 manager = Manager(app)
 
 migrate = Migrate(app, db)
